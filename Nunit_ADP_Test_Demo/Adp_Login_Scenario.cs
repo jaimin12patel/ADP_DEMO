c0c2df5f43aa4b5f981ffea1bada841e01057637
jaimin12patel <jaimin12patel@gmail.com>
2019-05-07 19:50:27 -0400

    final push

--- Nunit_ADP_Test_Demo/Adp_Login_Scenario.cs
+++ Nunit_ADP_Test_Demo/Adp_Login_Scenario.cs
@@ -1,18 +1,75 @@
+using System;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Nunit_ADP_Test_Demo.BaseClass;
+using System.Threading;
 
-namespace Tests
+namespace Nunit_ADP_Test_Demo.BaseClass
 {
-    public class Tests
+    [TestFixture]
+    public class Adp_Login_Scenario : Configuring_The_chrome
     {
-        [SetUp]
-        public void Setup()
+        public IWebElement email;
+        public IWebElement pass;
+        public IWebElement sub;
+        [Test]
+        //This Test case method will going to fail due the error messag pop-up 
+        public void Login_Negative_Empty_Username_pass()
         {
+            //Writing email with only space
+            email = driver.FindElement(By.Id("txtLoginId"));
+            email.SendKeys("");
+
+            //Click on Submit Button to verify the error message on different page.
+            sub = driver.FindElement(By.Id("btnNext"));
+            sub.Click();
+            Thread.Sleep(10000);
+
+            //Writing pass with only Space
+            //IWebElement pass = driver.FindElement(By.Id("btnNext"));
+            //pass.SendKeys("         ");
+
+            //Checking displed error message
+            var err = driver.FindElement(By.XPath("//*[@id=\"generic-message-dialog\"]/div/div[1]/p/span/span[2]")).Text;
+            Console.Write("Actual Message" + err);
+            string expectedTooltip = "Something isn't quite right";
+            Assert.AreEqual(expectedTooltip, err);
+        }
+        [Test]
+        public void Login_Positive_Username_pass()
+        {
+            //Writing valid USername 
+            email = driver.FindElement(By.XPath("//*[@id=\"txtLoginId\"]"));
+            email.SendKeys("Wavetest004");
+            Thread.Sleep(4000);
+
+            //Click on Next Button to verify username on next page
+            sub = driver.FindElement(By.XPath("//*[@id=\"btnNext\"]"));
+            sub.Click();
+            Thread.Sleep(4000);
+
+            //Checking username displayed on next page or not
+            var username = driver.FindElement(By.XPath("//*[@id=\"generic-message-dialog\"]/div/div[1]/p/span/span[2]")).Text;
+            Console.Write("Actual Message" + username);
+            string expectedTooltip = "Hello Wavetest004";
+            Assert.AreEqual(expectedTooltip, username);
+
+            //Writing valid USername 
+            pass = driver.FindElement(By.Id("PASSWORD"));
+            pass.SendKeys("test7711");
+
+            //Click on Next Button to verify username on next page
+            sub = driver.FindElement(By.Id("btnNext"));
+            sub.Click();
+            Thread.Sleep(4000);
+
+            //Checking welcome messge after successfully logged in 
+            var success = driver.FindElement(By.Id("UserWelcome")).Text;
+            Console.Write("Actual Message" + success);
+            string expectedsuccess = "Welcome, NileshFirst Contact Last4";
+            Assert.AreEqual(expectedsuccess, username);
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
 }
--- Nunit_ADP_Test_Demo/BaseClass/Configuring_The_chrome.cs
+++ Nunit_ADP_Test_Demo/BaseClass/Configuring_The_chrome.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
 namespace Nunit_ADP_Test_Demo.BaseClass
 {
-    public class EmptyClass
+    public class Configuring_The_chrome
     {
-        public EmptyClass()
+        public IWebDriver driver;
+        [SetUp]
+        public void Setup_Chrome()
         {
+            driver = new ChromeDriver("/Users/DIPAKPATEL/Documents/webdriver");
+            driver.Url = "https://runpayroll.adp.com";
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void close_chrome()
+        {
+            driver.Quit();
         }
     }
 }
